fd1b696e29b2081d9bf6efcb5bf2086530acaa2b
Charles Deuschle <cldeuschle@gmail.com>
2021-01-30 17:28:12 -0500

    Added Next and Previous buttons w/ deep links.

--- src/app/card-image/card-image.component.ts
+++ src/app/card-image/card-image.component.ts
@@ -1,6 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, PRIMARY_OUTLET, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Cards } from 'scryfall-sdk';
+
 import { DECKS } from '../decks';
-import { Cards, Card } from 'scryfall-sdk';
 import { LocalStorageService } from '../local-storage.service';
 
 @Component({
@@ -10,24 +14,57 @@ import { LocalStorageService } from '../local-storage.service';
 })
 export class CardImageComponent implements OnInit {
 
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router) { 
+      router.events.pipe(
+        filter(navEnd => navEnd instanceof  NavigationEnd))
+        .subscribe((navEnd: NavigationEnd) => {
+          const stuff2 = router.parseUrl(navEnd.url);
+          const segments = stuff2.root.children[PRIMARY_OUTLET].segments;
+          this.set = segments[1].path;
+          this.id  = +segments[2].path;
+          this.getCardImage();
+        })
+    }
 
   decks = DECKS;
   cardImage;
   set;
+  id;
 
   persist(key: string, value: any) {
     this.localStorageService.set(key, value);
   }
 
   ngOnInit(): void {
-    this.getCardImage();
     this.set = this.localStorageService.get('set');
+    this.id = +this.route.snapshot.paramMap.get('id');
+    this.getCardImage();
+  }
+
+  onChange(): void {
+    this.set = this.localStorageService.get('set');
+    
+    if (this.id == 0 && this.set != "")
+    {
+      Cards.bySet(this.set, 1).then(result => this.cardImage = result.image_uris.normal);
+    }
+    else
+    {
+      Cards.bySet(this.set, this.id).then(result => this.cardImage = result.image_uris.normal);
+    }
   }
 
   getCardImage(): void {
-    let card: Card;
-
-    Cards.bySet('khm', 1).then(result => this.cardImage = result.image_uris.normal);
+    if (this.id == 0 && this.set != "")
+    {
+      Cards.bySet(this.set, 1).then(result => this.cardImage = result.image_uris.normal);
+    }
+    else
+    {
+      Cards.bySet(this.set, this.id).then(result => this.cardImage = result.image_uris.normal);
+    }
   }
 }
